7d076e9372810b884b0d9a6d0154a96de36f63ea
Mahesh Varada <maheshchandra06@gmail.com>
2024-12-08 21:07:24 -0500

    commit part 1 and part2

---
+++ part1.py
@@ -0,0 +1,10 @@
+# Write a Python program to find the addition and subtraction of two numbers.
+# Ask the user to input two numbers (num1 and num2). Given those two numbers,
+# add them together to find the output. Also, subtract the two numbers to find the output.
+print('Enter the numbers for subtraction below.')
+num1 = int(input())
+num2 = int(input())
+print(num1,'-',num2,'=',num1-num2 )
+
+
+
---
+++ part2.py
@@ -0,0 +1,8 @@
+# Write a Python program to find the multiplication and division of two numbers.
+# Ask the user to input two numbers (num1 and num2). Given those two numbers,
+# multiply them together to find the output. Also, divide num1/num2 to find the output.
+print('Enter the numbers to find  multiplication and division below.')
+num1 = int(input())
+num2 = int(input())
+print(num1,'*',num2,'=',num1*num2 )
+print(num1,'/',num2,'=',num1/num2 )
