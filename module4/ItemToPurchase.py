1cbe719db21c1b42ad4440b9287f6f00c55e6a35
Mahesh Varada <maheshchandra06@gmail.com>
2025-01-12 14:56:28 -0500

    added module 4 assignment

---
+++ module4/ItemToPurchase.py
@@ -0,0 +1,38 @@
+# Online Shopping cart
+from warnings import catch_warnings
+
+
+# Step 1: Build the ItemToPurchase class
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = "none"
+        self.item_price = 0
+        self.item_quantity = 0
+
+    def cost(self):
+        return self.item_price * self.item_quantity
+
+    def print_item_cost(self):
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${self.cost()}")
+
+if __name__ == "__main__":
+
+    def purchaseItem(message):
+        print(message)
+        item = ItemToPurchase()
+        item.item_name = input("Enter the item name:\n")
+        item.item_price = float(input("Enter the item price:\n"))
+        item.item_quantity = int(input("Enter the item quantity:\n"))
+        print()
+        return item
+
+    # Step 2: Create two objects of the ItemToPurchase class
+    item1 = purchaseItem("Item1")
+    item2 = purchaseItem("Item2")
+
+    # Step 3: Add the costs of the two items together and output the total cost
+    print("TOTAL COST\n")
+    item1.print_item_cost()
+    item2.print_item_cost()
+
+    print(f"Total: ${item1.cost() + item2.cost()}")
